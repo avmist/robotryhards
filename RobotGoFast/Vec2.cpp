1565010d3641a6ed30faaa65482c3145fb752207
Christopher Paul Renden <renden.christopher@gmail.com>
2017-04-26 18:52:28 -0600

    Added fromPolar, fromPolarDeg, and angleTo functions in Vec2 class

--- RobotGoFast/Vec2.cpp
+++ RobotGoFast/Vec2.cpp
@@ -21,6 +21,10 @@ float Vec2::cross(Vec2 b) {
 	return this->x * b.y - this->y * b.x;
 }
 
+float Vec2::angleTo(Vec2 b) {
+	return acos(b.unit().dot(self.unit()));
+}
+
 Vec2 Vec2::operator* (float c) {
 	return Vec2(this->x * c, this->y * c);
 }
@@ -54,4 +58,12 @@ Vec2 Vec2::intersect(Vec2 p, Vec2 r, Vec2 q, Vec2 s) {
 	float rs = r.cross(s);			//local rs = r:cross(s)
 	Vec2 qp = q-p;					//local qp = q-p
 	return p + (r * qp.cross(s) / rs);	//p + r * qp:cross(s)/rs
+}
+
+Vec2 Vec2::fromPolar(double r, double a) {
+	return Vec2(r*cos(a), r*sin(a));
+}
+
+Vec2 Vec2::fromPolarDeg(double r, double d) {
+	return Vec2(r*cos(d / 180.0*PI), r*sin(d / 180.0*PI));
 }
--- RobotGoFast/Vec2.h
+++ RobotGoFast/Vec2.h
@@ -17,6 +17,7 @@ public:
   Vec2 operator- (Vec2 b);	//subtract 2 vectors, (vector from b to this)
   float dot(Vec2 b);		//dot product
   float cross(Vec2 b);		//2D cross product (area of a paralellogram formed by the two vectors)
+  float angleTo(vec2 b);	//angle from this to the given b
   
   Vec2 operator* (float c);	//scale by c
   Vec2 operator/ (float c);	//scale by 1/c
@@ -27,8 +28,9 @@ public:
   Vec2 right();	//rotate right a quarter turn
   Vec2 unit();		//vector which points in the same direction but has a magnitude of 1
   
-  static Vec2 intersect(Vec2 p, Vec2 r, Vec2 q, Vec2 s);
-
+  static Vec2 intersect(Vec2 p, Vec2 r, Vec2 q, Vec2 s);	//intersection of two vectors, r and s, with respective origin points p and q
+  static Vec2 fromPolar(double r, double a);	//Converts Radius and Angle to X and Y
+  static Vec2 fromPolarDeg(double r, double d);	//Same as fromPolar but uses degrees because I know Cody is a filthy 360 degree plebian
 };
 
 #endif
